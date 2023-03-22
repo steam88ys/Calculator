286f46121b59a3692a3b2a07f664fb93d7db44a2
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-03-23 08:59:58 +0900

    드라이브 예제 풀어보기

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -7,29 +7,67 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
-        Number obj1 = ctx.getBean("t1", Number.class);
-        System.out.println("순서 변경 결과: "+ obj1.getStr2()+", "+obj1.getStr1());
+        Number obj1 = ctx.getBean("t1", Number.class);  // 1번
+        System.out.println("\n순서 변경 결과: "+ obj1.getStr2()+", "+obj1.getStr1());
 
-        Number obj2 = ctx.getBean("t2", Number.class);
+        Number obj2 = ctx.getBean("t2", Number.class);  // 2번
         int sum = obj2.getSu1()+obj2.getSu2()+obj2.getSu3();
         System.out.println("\n합: "+ sum);
         System.out.println("평균: "+ sum/3);
 
-        Number obj3 = ctx.getBean("t3", Number.class);
-        if(obj3.getSu4() > obj3.getSu5()) System.out.println("1");
-        else if(obj3.getSu4() < obj3.getSu5()) System.out.println("0");
-        else System.out.println(obj3.getSu4()+" == "+obj3.getSu5());
+        Number obj3 = ctx.getBean("t3", Number.class);  // 3번
+        if(obj3.getSu4() > obj3.getSu5()) System.out.println("\n1");
+        else if(obj3.getSu4() < obj3.getSu5()) System.out.println("\n0");
+        else System.out.println("\n"+obj3.getSu4()+" == "+obj3.getSu5());
 
-        Number obj4 = ctx.getBean("t2", Number.class);
-        if (obj4.getSu6() % 2 == 0) {
-            System.out.println(obj4.getSu6());
+        Number obj4 = ctx.getBean("t2", Number.class);  // 4번
+        if (obj4.getSu1() % 2 == 0) {
+            System.out.println("\n"+obj4.getSu1());
         }
-        if (obj4.getSu7() % 2 == 0) {
-            System.out.println(obj4.getSu7());
+        if (obj4.getSu2() % 2 == 0) {
+            System.out.println(obj4.getSu2());
         }
-        if (obj4.getSu8() % 2 == 0) {
-            System.out.println(obj4.getSu8());
+        if (obj4.getSu3() % 2 == 0) {
+            System.out.println(obj4.getSu3()+"\n");
         }
+
+        Number obj5 = ctx.getBean("t4", Number.class);  // 5번
+        if(obj5.getSu9()/10 == obj5.getSu9()%10) System.out.println("10의 자리와 1의 자리가 같습니다.\n");
+        else System.out.println("10의 자리와 1의 자리가 다릅니다.\n");
+        
+        // 6번
+        Number obj6 = ctx.getBean("t6", Number.class);
+        int a = obj6.getSu1();
+        int b = obj6.getSu2();
+        int c = obj6.getSu3();
+        if(a*a == (b*b+c*c) || b*b == (a*a+c*c) || c*c == (a*a+b*b)) {
+            System.out.println("삼각형이 가능합니다.");
+        }else System.out.println("삼각형이 불가능합니다.");
+        
+        //7번 
+        Number obj7 = ctx.getBean("t7", Number.class);
+        int max = obj7.getSu1();
+        int min = obj7.getSu1();
+        if(max < obj7.getSu2()) max = obj7.getSu2();
+        else if(max < obj7.getSu3()) max = obj7.getSu2();
+        if(min > obj7.getSu2()) min = obj7.getSu2();
+        else if(min > obj7.getSu3()) min = obj7.getSu3();
+        System.out.println("\n최대점수: "+max+", 최소점수: "+min);
+        
+        // 8번
+        Number obj8 = ctx.getBean("t8", Number.class);
+        a = obj8.getSu10();
+        b = obj8.getSu11();
+        c = obj8.getSu12();
+        int d = obj8.getSu13();
+        int e = obj8.getSu14();
+        sum=0;
+        if(a>=20 && a<=50) sum+= a;
+        if(b>=20 && b<=50) sum+= b;
+        if(c>=20 && c<=50) sum+= c;
+        if(d>=20 && d<=50) sum+= d;
+        if(e>=20 && e<=50) sum+= e;
+        System.out.println("\n합계: "+sum+", 평균: "+sum/5);
 
         ctx.close();
     }
--- src/main/java/kr/hs/study/beans/Number.java
+++ src/main/java/kr/hs/study/beans/Number.java
@@ -10,6 +10,8 @@ public class Number {
     private int su4, su5;
 
     private int su6, su7, su8;
+    private int su9;
+    private int su10, su11, su12, su13, su14;
 
     public Number(String str1, String str2) {
         this.str1 = str1;
@@ -26,6 +28,19 @@ public class Number {
         this.su4 = su4;
         this.su5 = su5;
     }
+
+    public Number(int su9) {
+        this.su9 = su9;
+    }
+
+    public Number(int su10, int su11, int su12, int su13, int su14) {
+        this.su10 = su10;
+        this.su11 = su11;
+        this.su12 = su12;
+        this.su13 = su13;
+        this.su14 = su14;
+    }
+
 
 
 }
